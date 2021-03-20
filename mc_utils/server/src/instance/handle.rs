d993812c0324ef2eb9701c949b4dc0e4454f10fa
Inky-developer <developerinky@gmail.com>
2021-03-20 18:31:13 +0000

    Check whether java is installed

--- mc_utils/server/src/instance/handle.rs
+++ mc_utils/server/src/instance/handle.rs
@@ -3,7 +3,30 @@ use std::{
     process::{Child, Command, Stdio},
 };
 
+thread_local! {
+    static HAS_JAVA: bool = has_java();
+}
+
+/// Checks whether the java command is available on this system
+pub fn has_java() -> bool {
+    match Command::new("java").arg("-version").spawn() {
+        Ok(mut child) => match child.wait() {
+            Ok(status) => status.success(),
+            _ => false,
+        },
+        _ => false,
+    }
+}
+
 pub fn run_server(path: impl AsRef<Path>, args: &[&str]) -> std::io::Result<Child> {
+    HAS_JAVA.with(|java| {
+        if !java {
+            Err(std::io::Error::new(std::io::ErrorKind::NotFound, "The java command was not found on this system."))
+        } else {
+            Ok(())
+        }
+    })?;
+
     let path = path.as_ref();
     let dir = path
         .parent()
